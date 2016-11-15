706f909b4740aa12fa2ce25ea3879539010fd047
Pratheek Hegde <ptk609@gmail.com>
2016-11-15 18:18:47 +0530

    feat: added dummy core module

---
+++ src/client/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { LoggerService } from './logger.service';
+@NgModule({
+  imports: [
+    CommonModule // we use ngFor
+  ],
+  exports: [],
+  declarations: [],
+  providers: []
+})
+export class CoreModule { }
